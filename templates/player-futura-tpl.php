7771acec521755a4b2b574b0b6fc397486f3d3fe
Adrián Pardini <github@tangopardo.com.ar>
2020-05-12 16:08:23 -0300

    update plugin template to match new design

--- templates/player-futura-tpl.php
+++ templates/player-futura-tpl.php
@@ -1,19 +1,35 @@
 <?php
 $player_base_url = plugins_url('templates/', dirname(__FILE__));
 ?>
-<div class="player__container" id="<?php echo $player_id ?>">
-      <img src="<?php echo $player_base_url ?>vivo.svg"  class="player__branding" />
-      <div class="player__content">
-        <div class="player__control">
-        <img src="<?php echo $player_base_url ?>play.svg"  class="player__play"  />
-        <img src="<?php echo $player_base_url ?>pause.svg" class="player__pause" />
-        </div>
-        <div class="player__pad"> </div>
-        <div class="player__nowplaying">
-          <span class="player__currentshow">  </span>
+
+  <div class="player__container" id="<?php echo $player_id ?>">
+<!--      <img src="<?php echo $player_base_url ?>vivo.svg"  class="player__branding" /> -->
+
+      <div class="player__nowplaying">
+        <span class="player__currentshow"> </span>
+      </div>
+
+      <div>
+        <div class="player__branding__text">
+          Escuchá
+          <br />
+          <span class="brand__futura">
+            FUTURA
+          </span>
+          <br />
+          en VIVO
         </div>
       </div>
-    </div>
+
+      <div class="player__content">
+        <div class="player__control">
+          <div class="player__play" > <img src="<?php echo $player_base_url ?>play.svg" /> </div>
+          <div class="player__pause"> <img src="<?php echo $player_base_url ?>pause.svg"/> </div>
+        </div>
+        <div class="player__pad"> </div>
+      </div>
+  </div>
+
   <script type="text/javascript">
     var player = new FuturaPlayer(document.querySelector('#<?php echo $player_id?>'));
   </script>
