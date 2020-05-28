013e0b10a34325854bdf10055b8c1552faf97f5d
Adrián Pardini <github@tangopardo.com.ar>
2020-05-28 17:09:30 -0300

    player-futura: annotate elements for turbolinks
      - do not re-execute script
      - tag player as permanent element

--- templates/player-futura-tpl.php
+++ templates/player-futura-tpl.php
@@ -2,7 +2,7 @@
 $player_base_url = plugins_url('templates/', dirname(__FILE__));
 ?>
 
-  <div class="player__container" id="<?php echo $player_id ?>">
+  <div data-turbolinks-permanent class="player__container" id="<?php echo $player_id ?>">
 <!--      <img src="<?php echo $player_base_url ?>vivo.svg"  class="player__branding" /> -->
 
       <div class="player__nowplaying">
@@ -30,7 +30,7 @@ $player_base_url = plugins_url('templates/', dirname(__FILE__));
       </div>
   </div>
 
-  <script type="text/javascript">
+  <script type="text/javascript" data-turbolinks-eval="false">
     var player = new FuturaPlayer(document.querySelector('#<?php echo $player_id?>'));
   </script>
 
