a285e7cc35019aec9b35fd70ded498c8a5a61a97
Adrián Pardini <github@tangopardo.com.ar>
2020-09-11 12:11:32 -0300

    bump version

--- wp-futura-player.php
+++ wp-futura-player.php
@@ -4,7 +4,7 @@ Plugin Name:        FM Futura Player
 Plugin URI:         https://github.com/fm-futura/wp-futura-player
 GitHub Plugin URI:  https://github.com/fm-futura/wp-futura-player
 Description:        Reproductor para nuestro streaming
-Version:            20200514
+Version:            20200911
 Author:             FM Futura
 Author URI:         https://fmfutura.com.ar
 License:            AGPL-3.0
